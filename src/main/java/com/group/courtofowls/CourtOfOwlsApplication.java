4ee6a09b0005cbe46a8c837d700c6db20c8b3857
William <wrcolon@gmail.com>
2019-06-03 16:40:58 -0400

    added eclipse project

---
+++ src/main/java/com/group/courtofowls/CourtOfOwlsApplication.java
@@ -0,0 +1,13 @@
+package com.group.courtofowls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtOfOwlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtOfOwlsApplication.class, args);
+	}
+
+}
