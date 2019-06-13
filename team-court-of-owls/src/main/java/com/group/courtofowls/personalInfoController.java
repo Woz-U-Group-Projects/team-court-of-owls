d3113436d299d7e40dcb969464dbcb6bd5999e06
Sheldon WIlliams <43483242+swilliams23@users.noreply.github.com>
2019-06-13 03:36:02 -0500

    Add files via upload
    created java classes for mapping

---
+++ team-court-of-owls/src/main/java/com/group/courtofowls/personalInfoController.java
@@ -0,0 +1,40 @@
+package com.group.courtofowls;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/personalInfo")
+public class personalInfoController {
+	
+	@Autowired
+	FamilyTreeRepository individualRepository;
+	
+	@GetMapping("/")
+	public String index() {
+		
+		return "index";
+	}
+	
+	
+	@GetMapping("/info")
+	public List<personalInfo> get(){
+		return individualRepository.findAll();
+	}
+	
+	@PostMapping()
+	public ResponseEntity<personalInfo> addEntity(@RequestBody personalInfo info){
+		if(info.validModel()) {
+			individualRepository.save(info);
+			return ResponseEntity.ok(info);	
+		}
+		return ResponseEntity.badRequest().build();		
+	}
+}
